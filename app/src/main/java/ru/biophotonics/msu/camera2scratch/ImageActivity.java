d2ea9bbda132d9f6a8e74d9005ebd6aee083a1bd
byakimov <bp.yakimov@gmail.com>
2017-10-30 18:04:45 +0300

    Image Activity added

---
+++ app/src/main/java/ru/biophotonics/msu/camera2scratch/ImageActivity.java
@@ -0,0 +1,13 @@
+package ru.biophotonics.msu.camera2scratch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
