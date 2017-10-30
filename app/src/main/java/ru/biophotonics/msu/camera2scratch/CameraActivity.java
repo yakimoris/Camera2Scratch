a6e567145abd041fad84cf44c9dfc4e667590c98
byakimov <bp.yakimov@gmail.com>
2017-10-30 17:57:33 +0300

    First commit

---
+++ app/src/main/java/ru/biophotonics/msu/camera2scratch/CameraActivity.java
@@ -0,0 +1,19 @@
+package ru.biophotonics.msu.camera2scratch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
